c3879fefe289c31a571ab0f2b57f9f98bd62a416
Lisper-iMac <leyapin@gmail.com>
2019-02-26 10:35:48 +0800

    update

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,41 @@
 import React, {Component} from 'react';
-import './App.css';
+import css from './App.module.css';
 import Dialog from '../Dialog';
 
 class App extends Component {
 
+    btnClick() {
+        Dialog.show({
+            title: 'Current Time',
+            content: (new Date()).toLocaleString(),
+            bgClose: true,
+            onClose: () => {
+                console.log("onClose");
+            }
+        });
+    }
+
+    btnClick2() {
+        Dialog.show({
+            title: 'Current Time',
+            content: (new Date()).toLocaleString()
+        });
+
+        let timer = setInterval(() => {
+            Dialog.show({
+                content: (new Date()).toLocaleString(),
+                onClose: () => {
+                    clearInterval(timer);
+                }
+            });
+        }, 1000);
+    }
+
     render() {
         return (
-            <div className="App">
-                <button onClick={() => Dialog.show('Current Time', (new Date()).toLocaleString())}>Open Dialog</button>
+            <div className={css.App}>
+                <button onClick={this.btnClick.bind(this)}>Open Dialog</button>
+                <button onClick={this.btnClick2.bind(this)}>Open Dialog With real time</button>
             </div>
         );
     }
--- src/components/Dialog/dialog.js
+++ src/components/Dialog/dialog.js
@@ -9,12 +9,24 @@ class Dialog extends React.Component {
             open: false,
             show: false,
             title: '',
-            content: ''
+            content: '',
+            bgClose: true,
+            onClose: null
         }
     }
 
-    show(title, content) {
-        this.setState({open: true, title: title, content: content})
+    show(params) {
+        let {title, content, bgClose, onClose} = params;
+
+        let state = {open: true, bgClose, onClose};
+        if (title) {
+            state.title = title;
+        }
+        if (content) {
+            state.content = content;
+        }
+
+        this.setState(state);
         setTimeout(() => {
             this.setState({show: true});
         }, 10);
@@ -25,14 +37,36 @@ class Dialog extends React.Component {
         setTimeout(() => {
             this.setState({open: false});
         }, 210);
+        if (typeof this.state.onClose === 'function') {
+            this.state.onClose();
+        }
+    }
+
+    disableEvent(e) {
+        e.preventDefault();
+        e.stopPropagation();
+    }
+
+    bgClick() {
+        if (this.state.bgClose) {
+            this.close();
+        }
     }
 
     render() {
         return <div
-            className={css.dialogMask + ' ' + (this.state.open ? css.open : '') + ' ' + (this.state.show ? css.show : '')}>
+            className={css.dialogMask + ' ' + (this.state.open ? css.open : '') + ' ' + (this.state.show ? css.show : '')}
+            onMouseDown={this.disableEvent}
+            onMouseMove={this.disableEvent}
+            onWheel={this.disableEvent}
+            onClick={this.bgClick.bind(this)}
+        >
             <div className={css.dialog}>
                 <div className={css.dialogTitle}>{this.state.title}
-                    <div className={css.closeBtn} onClick={this.close.bind(this)}>X</div>
+                    <div className={css.closeBtn} onClick={this.close.bind(this)}>
+                        <i></i>
+                        <i></i>
+                    </div>
                 </div>
                 <div className={css.dialogContent}>{this.state.content}</div>
             </div>
